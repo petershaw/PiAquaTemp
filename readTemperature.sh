d4931ad1dca4715167296083ff158010ea4654fb
Kris <kris@opentabs.de>
2014-09-09 23:54:19 +0200

    read all sensors

--- readTemperature.sh
+++ readTemperature.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# get sonsor list
+sensorlist=`ls -v /sys/bus/w1/devices/ | grep ^[0-9]`
+
 while [ 1 ]
 do
-  temp=`cat /sys/bus/w1/devices/28-00042d50b5ff/w1_slave | sed -n 2p | cut -d' ' -f10 | cut -d'=' -f2`
-  ./bin/log --sensor=00042d50b5ff --value=$temp
-  sleep 30
+  for S in $sensorlist
+  do
+    temp=`cat /sys/bus/w1/devices/${S}/w1_slave | sed -n 2p | cut -d' ' -f10 | cut -d'=' -f2`
+    echo --sensor=00042d50b5ff --value=$temp
+  done
+  sleep 15
 done
