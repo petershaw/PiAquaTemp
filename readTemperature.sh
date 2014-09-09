2e2f277365b02dd1d45100340020e777b7a99ef2
Kris <kris@opentabs.de>
2014-09-10 00:02:46 +0200

    fix sensor name

--- readTemperature.sh
+++ readTemperature.sh
@@ -8,7 +8,7 @@ do
   for S in $sensorlist
   do
     temp=`cat /sys/bus/w1/devices/${S}/w1_slave | sed -n 2p | cut -d' ' -f10 | cut -d'=' -f2`
-    echo --sensor=00042d50b5ff --value=$temp
+    ./bin/log --sensor=${S} --value=$temp
   done
   sleep 15
 done
