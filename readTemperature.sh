f98f1c4068ad549d1958dd0cf7a2b2043d269410
Kris <kris@opentabs.de>
2014-09-09 23:39:46 +0200

    loop temperature forever

--- readTemperature.sh
+++ readTemperature.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-temp=`cat /sys/bus/w1/devices/28-00042d50b5ff/w1_slave | sed -n 2p | cut -d' ' -f10 | cut -d'=' -f2`
-./bin/log --sensor=00042d50b5ff --value=$temp
+while [ 1 ]
+do
+  temp=`cat /sys/bus/w1/devices/28-00042d50b5ff/w1_slave | sed -n 2p | cut -d' ' -f10 | cut -d'=' -f2`
+  ./bin/log --sensor=00042d50b5ff --value=$temp
+  sleep 30
+done
