bbaaae944671c6535b983b91270bde46754ea72d
Peter Shaw <unthoughted@googlemail.com>
2014-09-11 11:07:58 +0200

    get temperaturesensor list inside the loop

--- readTemperature.sh
+++ readTemperature.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# get sonsor list
-sensorlist=`ls -v /sys/bus/w1/devices/ | grep ^[0-9]`
-
 while [ 1 ]
 do
+  sensorlist=`ls -v /sys/bus/w1/devices/ | grep ^[0-9]`
   for S in $sensorlist
   do
     temp=`cat /sys/bus/w1/devices/${S}/w1_slave | sed -n 2p | cut -d' ' -f10 | cut -d'=' -f2`
