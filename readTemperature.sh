69eae15571e7519f2aa0449cf078cb391a2b66b9
Peter Shaw <unthoughted@googlemail.com>
2014-10-24 23:05:00 +0200

    outputting

--- readTemperature.sh
+++ readTemperature.sh
@@ -5,8 +5,8 @@ do
   sensorlist=`ls -v /sys/bus/w1/devices/ | grep ^[0-9]`
   for S in $sensorlist
   do
-  	echo "Read sensor ${S}"
     temp=`cat /sys/bus/w1/devices/${S}/w1_slave | sed -n 2p | cut -d' ' -f10 | cut -d'=' -f2`
+	echo "Read sensor ${S} with a temperature of ${temp}"
     ./bin/log --sensor=${S} --value=$temp
   done
   sleep 15
